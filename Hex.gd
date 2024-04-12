5207ef054314ce44c8628c59c06154127137010f
Joao.Capello <joao.capello@gsuite.iff.edu.br>
2024-04-12 03:04:36 -0300

    refator: hex mais consistente e robusto

--- Hex.gd
+++ Hex.gd
@@ -1,7 +1,25 @@
 extends Node2D
 
-var size : float = 0
 
-func initialize(_pos : Vector2, _size : float):
-	global_position = _pos
-	size = _size
+@onready var poly : Polygon2D = $Polygon2D
+@onready var insides : Polygon2D = $Polygon2DBorder
+
+
+@export_range(0.0, 1.0, 0.05) var insidesScale : float = 0.1
+@export var IsOutsideVisible : bool = false
+
+
+var radius : float = 100			#center to tip distance
+var t2t : float = radius * 2		#tip to tip distance
+var f2f : float = radius * 1.73	#flat to flat distance
+
+var corners : PackedVector2Array = [Vector2(-radius,0), Vector2(-t2t*0.25,-f2f*0.5),
+									Vector2(t2t*0.25,-f2f*0.5), Vector2(radius,0), 
+									Vector2(t2t*0.25,f2f*0.5),Vector2(-t2t*0.25,f2f*0.5)]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	poly.polygon = corners
+	insides.polygon = corners
+	poly.visible = IsOutsideVisible
+	insides.scale = Vector2(insidesScale, insidesScale)
--- HexGrid.gd
+++ HexGrid.gd
@@ -1,36 +1,40 @@
 extends Node2D
 
-@export var size : float = 100			#center to tip
+@export var radius : float = 100			#center to tip
+@export var xMax : int = 4				#max amount of hexes in a row
+@export var yMax : int = 4				#max amount of rows
+@export var origin : Vector2 = Vector2(0,0)
 
-var hex : PackedScene = preload("res://hex.tscn")
+var hex : PackedScene = preload("res://Hex.tscn")
 
-var origin : Vector2 = Vector2(0,0)
-var outerR : float = 2 * size 			#tip to tip
-var innerR : float = 1.7 * size 		#flat to flat
+var t2t : float = 2 * radius 			#tip to tip
+var f2f : float = 1.73 * radius 		#flat to flat
 var orientation : String = "flat-top"
-var outerOffset : float = (3 * size)/4
-var innerOffset : float = 1.7 * size
-var rowOffset : float = size
+var t2tOffset : float = 0.75 * t2t 
+var f2fOffset : float = f2f
+var rowOffset : float = f2f/2
 var grid : Array = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	global_position=origin
-	var xMax = 4;
-	var yMax = 2;
-	for x in range(0,xMax):
-		grid.append([])
-		for y in range(0,yMax):
-			var newHex = hex.instantiate()
-			if ((x%2) == 0):
-				newHex.initialize(Vector2(origin.x+x*outerR,origin.y+y*innerR), size)
-			elif ((x%2)!=0):
-				newHex.initialize(Vector2(origin.x+x*outerR,origin.y+y*innerR+rowOffset), size)
-			add_child(newHex)
-			grid[x].append(newHex)
+	create_grid(xMax, yMax)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+
+func create_grid(_xMax : int, _yMax : int):
+	global_position = origin
+	for x in range(0,xMax):
+		grid.append([])
+		for y in range(0,yMax):
+			var newHex = hex.instantiate()
+			if ((x%2) == 0):
+				newHex.position = Vector2(x*t2tOffset,y*f2fOffset)
+			elif ((x%2)!=0):
+				newHex.position = Vector2(x*t2tOffset,y*f2fOffset+rowOffset)
+			add_child(newHex)
+			grid[x].append(newHex)
